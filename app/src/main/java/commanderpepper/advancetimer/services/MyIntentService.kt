ae8295d4fb053a2d6fa78b0529a09aad4b7d9818
commanderpepepr <ahmadhumza7@gmail.com>
2019-09-29 22:46:01 -0400

    Added check to make notifications work with a service

--- app/src/main/java/commanderpepper/advancetimer/services/MyIntentService.kt
+++ app/src/main/java/commanderpepper/advancetimer/services/MyIntentService.kt
@@ -1,18 +1,16 @@
 package commanderpepper.advancetimer.services
 
-import android.app.IntentService
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
-import kotlinx.coroutines.*
-import android.R
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import android.app.PendingIntent
-import android.content.Context
 import commanderpepper.advancetimer.ui.MainActivity
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import kotlinx.coroutines.*
 
 
 /**
@@ -35,8 +33,15 @@ class MyIntentService : IntentService("MyIntentService") {
             notificationIntent, 0
         )
 
-        val notification = NotificationCompat.Builder(this, "TEST")
-            .setSmallIcon(commanderpepper.advancetimer.R.drawable.ic_launcher_background)
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "My Background Service")
+            } else {
+                ""
+            }
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(commanderpepper.advancetimer.R.mipmap.ic_launcher)
             .setContentTitle("My Awesome App")
             .setContentText("Doing some work...")
             .setContentIntent(pendingIntent).build()
@@ -56,6 +61,19 @@ class MyIntentService : IntentService("MyIntentService") {
 
 
         Toast.makeText(applicationContext, string, Toast.LENGTH_LONG).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = NotificationChannel(
+            channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE
+        )
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
     }
 
 }
