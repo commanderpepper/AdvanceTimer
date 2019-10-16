1ddc866e0c589a478b51788556120c7aae1fbb39
commanderpepepr <ahmadhumza7@gmail.com>
2019-10-16 16:05:19 -0400

    Fragment is inserted with a fragment transaction

--- app/src/main/java/commanderpepper/advancetimer/ui/EntryActivity.kt
+++ app/src/main/java/commanderpepper/advancetimer/ui/EntryActivity.kt
@@ -3,6 +3,7 @@ package commanderpepper.advancetimer.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import commanderpepper.advancetimer.R
+import commanderpepper.advancetimer.ui.fragments.AlarmList
 import timber.log.Timber
 
 class EntryActivity : AppCompatActivity() {
@@ -11,5 +12,11 @@ class EntryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry)
         Timber.d("I'm alive!")
+
+        val alarmList = AlarmList.newInstance("A", "B")
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.entry_fragment_placeholder, alarmList)
+        fragmentTransaction.commit()
     }
 }
