f1a5a92e0125c83444d6e1a9df5981dbb8bb5587
commanderpepepr <ahmadhumza7@gmail.com>
2019-10-19 23:13:19 -0400

    Implemented the method in the EntryWorkflow class

--- app/src/main/java/commanderpepper/advancetimer/ui/entry/EntryLayoutRunner.kt
+++ app/src/main/java/commanderpepper/advancetimer/ui/entry/EntryLayoutRunner.kt
@@ -2,10 +2,15 @@ package commanderpepper.advancetimer.ui.entry
 
 import android.view.View
 import com.squareup.workflow.ui.LayoutRunner
+import com.squareup.workflow.ui.ViewBinding
+import commanderpepper.advancetimer.R
 
 class EntryLayoutRunner(view: View): LayoutRunner<EntryWorkflow.Rendering> {
     override fun showRendering(rendering: EntryWorkflow.Rendering) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    companion object : ViewBinding<EntryWorkflow.Rendering> by LayoutRunner.Companion.bind(
+        R.layout.activity_entry, ::EntryLayoutRunner
+    )
 }
--- app/src/main/java/commanderpepper/advancetimer/ui/entry/EntryWorkflow.kt
+++ app/src/main/java/commanderpepper/advancetimer/ui/entry/EntryWorkflow.kt
@@ -3,10 +3,25 @@ package commanderpepper.advancetimer.ui.entry
 import com.squareup.workflow.RenderContext
 import com.squareup.workflow.Snapshot
 import com.squareup.workflow.StatefulWorkflow
+import com.squareup.workflow.parse
+import javax.sql.StatementEvent
 
 object EntryWorkflow :
+
+/**
+ * @param PropsT I don't think anything needs to be passed to the EntryWorkflow as
+ * this is the main activity users will enter the app with. This may change in the future.
+ * There are no parent workflows for the Entry Workflow, none that I can think of.
+ *
+ * @param State The internal state is that the entry activity
+ */
     StatefulWorkflow<Unit, EntryWorkflow.State, Nothing, EntryWorkflow.Rendering>() {
 
+    /**
+     * Alarms = 1
+     * Timers = 2
+     * Options = 3
+     */
     enum class State {
         Alarms, Timers, Options
     }
@@ -15,9 +30,14 @@ object EntryWorkflow :
 
     }
 
-    override fun initialState(props: Unit, snapshot: Snapshot?): State {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun initialState(props: Unit, snapshot: Snapshot?) =
+        snapshot?.bytes?.parse { source ->
+            when (source.readInt()) {
+                1 -> State.Alarms
+                2 -> State.Timers
+                else -> State.Options
+            }
+        } ?: State.Alarms
 
     override fun render(
         props: Unit,
@@ -28,7 +48,11 @@ object EntryWorkflow :
     }
 
     override fun snapshotState(state: State): Snapshot {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return when (state) {
+            State.Alarms -> Snapshot.of(1)
+            State.Timers -> Snapshot.of(2)
+            else -> Snapshot.of(3)
+        }
     }
 
 }
