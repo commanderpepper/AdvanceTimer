b53a6e7a97588a1ed339a248e21dd139c0218249
commanderpepepr <ahmadhumza7@gmail.com>
2019-11-08 22:59:49 -0500

    Starting to work on the AlarmCreator test class

--- app/src/androidTest/java/commanderpepper/advancetimer/AlarmCreatorTest.kt
+++ app/src/androidTest/java/commanderpepper/advancetimer/AlarmCreatorTest.kt
@@ -1,24 +1,47 @@
 package commanderpepper.advancetimer
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
 import commanderpepper.advancetimer.alarmcreation.AlarmCreator
+import commanderpepper.advancetimer.receivers.AlarmReceiver
+import org.hamcrest.CoreMatchers
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert.assertThat
 
 
 /**
  * Test Alarm Creation
  */
 @RunWith(AndroidJUnit4::class)
-class AlarmCreatorTest{
-    private lateinit var context : Context
+class AlarmCreatorTest {
+    private lateinit var context: Context
     private lateinit var alarmCreator: AlarmCreator
 
     @Before
-    fun init(){
+    fun init() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
         alarmCreator = AlarmCreator(context)
+    }
+
+    @Test
+    fun createPendingIntent_RetrievePendingIntent_CheckIfEqual() {
+        val sourceIntent = Intent(context, AlarmReceiver::class.java)
+            .putExtra("Source", "Test")
+
+        val alarmIntent: PendingIntent =
+            PendingIntent.getActivities(context, 0, arrayOf(sourceIntent), 0)
+
+//        alarmCreator.makeAlarm(alarmIntent, 10000L)
+
+        val retrievedIntent =
+            PendingIntent.getActivities(context, 0, arrayOf(sourceIntent), 0)
+
+
+        assertThat(alarmIntent, CoreMatchers.equalTo(retrievedIntent))
     }
 }
