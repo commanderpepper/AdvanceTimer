ce00549e95909f944052e91ec76f3a4d8ece5b09
commanderpepepr <ahmadhumza7@gmail.com>
2020-02-23 22:41:47 -0500

    The app is now able to start an standalone activity.

--- app/src/main/java/commanderpepper/advancetimer/receivers/MyReceiver.kt
+++ app/src/main/java/commanderpepper/advancetimer/receivers/MyReceiver.kt
@@ -3,6 +3,8 @@ package commanderpepper.advancetimer.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import commanderpepper.advancetimer.ui.dismisstimer.DismissTimer
+import commanderpepper.advancetimer.ui.entry.AlarmActivityTest
 import timber.log.Timber
 
 class MyReceiver : BroadcastReceiver() {
@@ -10,5 +12,8 @@ class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         Timber.d("Is this working?")
+        val intent = Intent(context, DismissTimer::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context.startActivity(intent)
     }
 }
---
+++ app/src/main/java/commanderpepper/advancetimer/ui/dismisstimer/DismissTimer.kt
@@ -0,0 +1,13 @@
+package commanderpepper.advancetimer.ui.dismisstimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import commanderpepper.advancetimer.R
+
+class DismissTimer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dismiss_timer)
+    }
+}
